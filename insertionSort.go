6cbff2893520bfb03e5023e6309ea0e21e617130
Kazuki Matsumoto <katz.matz.dev@gmail.com>
2020-06-06 17:47:56 +0900

    add float type to insertion sort

--- insertionSort.go
+++ insertionSort.go
@@ -41,3 +41,41 @@ func InsertionSortIntDsc(slice []int) []int {
 	}
 	return slice
 }
+
+func InsertionSortFloat64(slice []float64, ascOrDsc string) []float64 {
+	if strings.EqualFold("asc", ascOrDsc) {
+		return InsertionSortFloat64Asc(slice)
+	} else if strings.EqualFold("dsc", ascOrDsc) {
+		return InsertionSortFloat64Dsc(slice)
+	} else {
+		return nil
+	}
+}
+
+func InsertionSortFloat64Asc(slice []float64) []float64 {
+	var size int = len(slice)
+
+	var key int = 0
+	for i := 1; i < size; i++ {
+		key = i
+		for (key > 0) && (slice[key] < slice[key-1]) {
+			slice[key], slice[key-1] = slice[key-1], slice[key]
+			key--
+		}
+	}
+	return slice
+}
+
+func InsertionSortFloat64Dsc(slice []float64) []float64 {
+	var size int = len(slice)
+
+	var key int = 0
+	for i := 1; i < size; i++ {
+		key = i
+		for (key > 0) && (slice[key] > slice[key-1]) {
+			slice[key], slice[key-1] = slice[key-1], slice[key]
+			key--
+		}
+	}
+	return slice
+}
