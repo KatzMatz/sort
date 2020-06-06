c62ae913017215fb39fbe45408b9bd08d4e37cec
Kazuki Matsumoto <katz.matz.dev@gmail.com>
2020-06-06 19:17:58 +0900

    add float type to quick sort

--- quickSort.go
+++ quickSort.go
@@ -71,3 +71,71 @@ func QuickSortIntDsc(slice []int) []int {
 	}
 
 }
+
+func QuickSortFloat64(slice []float64, ascOrDsc string) []float64 {
+	if strings.EqualFold("asc", ascOrDsc) {
+		return QuickSortFloat64Asc(slice)
+	} else if strings.EqualFold("dsc", ascOrDsc) {
+		return QuickSortFloat64Dsc(slice)
+	} else {
+		return nil
+	}
+}
+
+func QuickSortFloat64Asc(slice []float64) []float64 {
+	var size int = len(slice)
+	if size > 1 {
+		var sorted []float64 = make([]float64, size)
+
+		var pivot float64 = slice[0]
+		var left []float64
+		var right []float64
+
+		for _, item := range slice[1:] {
+			if item <= pivot {
+				left = append(left, item)
+			} else {
+				right = append(right, item)
+			}
+		}
+		left = QuickSortFloat64Asc(left)
+		right = QuickSortFloat64Asc(right)
+
+		sorted = append(left, pivot)
+		sorted = append(sorted, right...)
+
+		return sorted
+	} else {
+		return slice
+	}
+
+}
+
+func QuickSortFloat64Dsc(slice []float64) []float64 {
+	var size int = len(slice)
+	if size > 1 {
+		var sorted []float64 = make([]float64, size)
+
+		var pivot float64 = slice[0]
+		var left []float64
+		var right []float64
+
+		for _, item := range slice[1:] {
+			if item >= pivot {
+				left = append(left, item)
+			} else {
+				right = append(right, item)
+			}
+		}
+		left = QuickSortFloat64Dsc(left)
+		right = QuickSortFloat64Dsc(right)
+
+		sorted = append(left, pivot)
+		sorted = append(sorted, right...)
+
+		return sorted
+	} else {
+		return slice
+	}
+
+}
