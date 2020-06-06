33ee19ad60503d646363f709a9065a7ba8f8ea23
Kazuki Matsumoto <katz.matz.dev@gmail.com>
2020-06-06 17:21:03 +0900

    add float64 type to bubble sort

--- bubbleSort.go
+++ bubbleSort.go
@@ -41,3 +41,41 @@ func BubbleSortIntDsc(slice []int) []int {
 
 	return slice
 }
+
+func BubbleSortFloat64(slice []float64, ascOrDsc string) []float64 {
+	if strings.EqualFold("asc", ascOrDsc) {
+		return BubbleSortFloat64Asc(slice)
+	} else if strings.EqualFold("dsc", ascOrDsc) {
+		return BubbleSortFloat64Dsc(slice)
+	} else {
+		return nil
+	}
+}
+
+func BubbleSortFloat64Asc(slice []float64) []float64 {
+	var n int = len(slice)
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1-i; j++ {
+			if slice[j] > slice[j+1] {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+			}
+		}
+	}
+
+	return slice
+}
+
+func BubbleSortFloat64Dsc(slice []float64) []float64 {
+	var n int = len(slice)
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1-i; j++ {
+			if slice[j] > slice[j+1] {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+			}
+		}
+	}
+
+	return slice
+}
