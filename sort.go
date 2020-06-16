4eafb0a0f2232504f83cd6c1627063da887f9884
Kazuki Matsumoto <katz.matz.dev@gmail.com>
2020-06-16 23:11:47 +0900

    change reverse function

--- sort.go
+++ sort.go
@@ -19,3 +19,18 @@ type Float64Slice []float64
 func (p Float64Slice) Len() int           { return len(p) }
 func (p Float64Slice) Less(i, j int) bool { return p[i] < p[j] }
 func (p Float64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func reverseSlice(data SortInterface) {
+	var size int = data.Len()
+	for i := 0; i < size/2; i++ {
+		data.Swap(i, size-i-1)
+	}
+}
+
+func reverseSliceInt(slice []int) {
+	reverseSlice(IntSlice(slice))
+}
+
+func reverseSliceFloat64(slice []float64) {
+	reverseSlice(Float64Slice(slice))
+}
