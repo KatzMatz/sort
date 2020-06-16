410ef3abff0d94601575b6d0d1d1ce92ee1bd308
Kazuki Matsumoto <katz.matz.dev@gmail.com>
2020-06-16 23:22:21 +0900

    add interface

--- insertionSort.go
+++ insertionSort.go
@@ -4,78 +4,70 @@ import (
 	"strings"
 )
 
-func InsertionSortInt(slice []int, ascOrDsc string) []int {
+func InsertionSortInt(slice []int, ascOrDsc string) bool {
 	if strings.EqualFold("asc", ascOrDsc) {
-		return InsertionSortIntAsc(slice)
+		InsertionSortIntAsc(slice)
+		return true
 	} else if strings.EqualFold("dsc", ascOrDsc) {
-		return InsertionSortIntDsc(slice)
+		InsertionSortIntDsc(slice)
+		return true
 	} else {
-		return nil
+		return false
 	}
 }
 
-func InsertionSortIntAsc(slice []int) []int {
-	var size int = len(slice)
-
-	var key int = 0
-	for i := 1; i < size; i++ {
-		key = i
-		for (key > 0) && (slice[key] < slice[key-1]) {
-			slice[key], slice[key-1] = slice[key-1], slice[key]
-			key--
-		}
-	}
-	return slice
+func InsertionSortIntAsc(slice []int) {
+	InsertionSortAsc(IntSlice(slice))
 }
 
-func InsertionSortIntDsc(slice []int) []int {
-	var size int = len(slice)
-
-	var key int = 0
-	for i := 1; i < size; i++ {
-		key = i
-		for (key > 0) && (slice[key] > slice[key-1]) {
-			slice[key], slice[key-1] = slice[key-1], slice[key]
-			key--
-		}
-	}
-	return slice
+func InsertionSortIntDsc(slice []int) {
+	InsertionSortDsc(IntSlice(slice))
 }
 
-func InsertionSortFloat64(slice []float64, ascOrDsc string) []float64 {
+func InsertionSortFloat64(slice []float64, ascOrDsc string) bool {
 	if strings.EqualFold("asc", ascOrDsc) {
-		return InsertionSortFloat64Asc(slice)
+		InsertionSortFloat64Asc(slice)
+		return true
 	} else if strings.EqualFold("dsc", ascOrDsc) {
-		return InsertionSortFloat64Dsc(slice)
+		InsertionSortFloat64Dsc(slice)
+		return true
 	} else {
-		return nil
+		return false
 	}
 }
 
-func InsertionSortFloat64Asc(slice []float64) []float64 {
-	var size int = len(slice)
-
-	var key int = 0
-	for i := 1; i < size; i++ {
-		key = i
-		for (key > 0) && (slice[key] < slice[key-1]) {
-			slice[key], slice[key-1] = slice[key-1], slice[key]
-			key--
-		}
-	}
-	return slice
+func InsertionSortFloat64Asc(slice []float64) {
+	InsertionSortAsc(Float64Slice(slice))
 }
 
-func InsertionSortFloat64Dsc(slice []float64) []float64 {
-	var size int = len(slice)
+func InsertionSortFloat64Dsc(slice []float64) {
+	InsertionSortDsc(Float64Slice(slice))
+}
+
+func InsertionSortAsc(data SortInterface) {
+	var size int = data.Len()
 
 	var key int = 0
+
 	for i := 1; i < size; i++ {
 		key = i
-		for (key > 0) && (slice[key] > slice[key-1]) {
-			slice[key], slice[key-1] = slice[key-1], slice[key]
+		for (key > 0) && (data.Less(key, key-1)) {
+			data.Swap(key, key-1)
 			key--
 		}
 	}
-	return slice
+}
+
+func InsertionSortDsc(data SortInterface) {
+	var size int = data.Len()
+
+	var key int = 0
+
+	for i := 1; i < size; i++ {
+		key = i
+		for (key > 0) && (data.Less(key-1, key)) {
+			data.Swap(key, key-1)
+			key--
+		}
+	}
 }
