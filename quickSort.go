a8ae94074a95d0cbc9499de27152a0866d4973fa
KatzMatz <katz.matz.dev@gmail.com>
2020-06-27 16:14:43 +0900

    modify quicksort

--- quickSort.go
+++ quickSort.go
@@ -4,170 +4,92 @@ import (
 	"strings"
 )
 
-func QuickSortInt(slice []int, ascOrDsc string) []int {
+func QuickSortInt(slice []int, ascOrDsc string) bool {
 	if strings.EqualFold("asc", ascOrDsc) {
-		return QuickSortIntAsc(slice)
+		QuickSortAscInt(slice)
+		return true
 	} else if strings.EqualFold("dsc", ascOrDsc) {
-		return QuickSortIntDsc(slice)
+		QuickSortDscInt(slice)
+		return true
 	} else {
-		return nil
+		return false
 	}
 }
 
-func QuickSortIntAsc(slice []int) []int {
-	var size int = len(slice)
-	if size > 1 {
-		var sorted []int = make([]int, size)
-
-		var pivot int = slice[0]
-		var left []int
-		var right []int
-
-		for _, item := range slice[1:] {
-			if item <= pivot {
-				left = append(left, item)
-			} else {
-				right = append(right, item)
-			}
-		}
-		left = QuickSortIntAsc(left)
-		right = QuickSortIntAsc(right)
-
-		sorted = append(left, pivot)
-		sorted = append(sorted, right...)
-
-		return sorted
-	} else {
-		return slice
-	}
-
-}
-
-func QuickSortIntDsc(slice []int) []int {
-	var size int = len(slice)
-	if size > 1 {
-		var sorted []int = make([]int, size)
-
-		var pivot int = slice[0]
-		var left []int
-		var right []int
-
-		for _, item := range slice[1:] {
-			if item >= pivot {
-				left = append(left, item)
-			} else {
-				right = append(right, item)
-			}
-		}
-		left = QuickSortIntDsc(left)
-		right = QuickSortIntDsc(right)
-
-		sorted = append(left, pivot)
-		sorted = append(sorted, right...)
-
-		return sorted
-	} else {
-		return slice
-	}
-
-}
-
-func QuickSortFloat64(slice []float64, ascOrDsc string) []float64 {
+func QuickSortFloat64(slice []float64, ascOrDsc string) bool {
 	if strings.EqualFold("asc", ascOrDsc) {
-		return QuickSortFloat64Asc(slice)
+		QuickSortAscFloat64(slice)
+		return true
 	} else if strings.EqualFold("dsc", ascOrDsc) {
-		return QuickSortFloat64Dsc(slice)
+		QuickSortDscFloat64(slice)
+		return true
 	} else {
-		return nil
+		return false
 	}
 }
 
-func QuickSortFloat64Asc(slice []float64) []float64 {
-	var size int = len(slice)
-	if size > 1 {
-		var sorted []float64 = make([]float64, size)
+func separateByPivotAsc(data SortInterface, startIdx int, endIdx int) int {
+	var midIdx int = startIdx
 
-		var pivot float64 = slice[0]
-		var left []float64
-		var right []float64
-
-		for _, item := range slice[1:] {
-			if item <= pivot {
-				left = append(left, item)
-			} else {
-				right = append(right, item)
-			}
+	for i := startIdx + 1; i < endIdx; i++ {
+		if data.Less(i, endIdx) {
+			data.Swap(i, midIdx)
+			midIdx++
 		}
-		left = QuickSortFloat64Asc(left)
-		right = QuickSortFloat64Asc(right)
-
-		sorted = append(left, pivot)
-		sorted = append(sorted, right...)
-
-		return sorted
-	} else {
-		return slice
 	}
-
+	data.Swap(midIdx, endIdx)
+	return midIdx
 }
 
-func QuickSortFloat64Dsc(slice []float64) []float64 {
-	var size int = len(slice)
-	if size > 1 {
-		var sorted []float64 = make([]float64, size)
+func separateByPivotDsc(data SortInterface, startIdx int, endIdx int) int {
+	var midIdx int = startIdx
 
-		var pivot float64 = slice[0]
-		var left []float64
-		var right []float64
-
-		for _, item := range slice[1:] {
-			if item >= pivot {
-				left = append(left, item)
-			} else {
-				right = append(right, item)
-			}
+	for i := startIdx + 1; i < endIdx; i++ {
+		if data.Less(endIdx, i) {
+			data.Swap(i, midIdx)
+			midIdx++
 		}
-		left = QuickSortFloat64Dsc(left)
-		right = QuickSortFloat64Dsc(right)
-
-		sorted = append(left, pivot)
-		sorted = append(sorted, right...)
-
-		return sorted
-	} else {
-		return slice
 	}
-
+	data.Swap(midIdx, endIdx)
+	return midIdx
 }
 
-func separatePivot(data SortInterface, startIdx int, endIdx int) int {
-
+func quickSortAscSub(data SortInterface, startIdx int, endIdx int) {
+	if startIdx < endIdx {
+		pivotIdx := separateByPivotAsc(data, startIdx, endIdx)
+		quickSortAscSub(data, startIdx, pivotIdx)
+		quickSortAscSub(data, pivotIdx+1, endIdx)
+	}
 }
 
-func QuickSortAsc(data SortInterface, startIdx, endIdx int) {
-	var size int = endIdx - startIdx
-	if size > 1 {
-		// var sorted []float64 = make([]float64, size)
-
-		var pivot float64 = slice[0]
-		var left []float64
-		var right []float64
-
-		for _, item := range slice[1:] {
-			if item <= pivot {
-				left = append(left, item)
-			} else {
-				right = append(right, item)
-			}
-		}
-		left = QuickSortFloat64Asc(left)
-		right = QuickSortFloat64Asc(right)
-
-		sorted = append(left, pivot)
-		sorted = append(sorted, right...)
-
-		return sorted
-	} else {
-		return slice
+func quickSortDscSub(data SortInterface, startIdx int, endIdx int) {
+	if startIdx < endIdx {
+		pivotIdx := separateByPivotDsc(data, startIdx, endIdx)
+		quickSortDscSub(data, startIdx, pivotIdx)
+		quickSortDscSub(data, pivotIdx+1, endIdx)
 	}
+}
+
+func quickSortAsc(data SortInterface) {
+	quickSortAscSub(data, 0, data.Len()-1)
+}
+
+func quickSortDsc(data SortInterface) {
+	quickSortDscSub(data, 0, data.Len()-1)
+}
+
+func QuickSortAscInt(slice []int) {
+	quickSortAsc(IntSlice(slice))
+}
+
+func QuickSortDscInt(slice []int) {
+	quickSortDsc(IntSlice(slice))
+}
+
+func QuickSortAscFloat64(slice []float64) {
+	quickSortAsc(Float64Slice(slice))
+}
+
+func QuickSortDscFloat64(slice []float64) {
+	quickSortDsc(Float64Slice(slice))
 }
